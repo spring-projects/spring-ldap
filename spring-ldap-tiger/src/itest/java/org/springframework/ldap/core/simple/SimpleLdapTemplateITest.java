f4c66734ace96b104257685f0b6cb2b27a281103
Mattias Arthursson <mattias.arthursson@gmail.com>
2007-10-23 18:09:51 +0000

    Made spring-ldap-tiger tests run with different LDAP server port to avoid collision with port used in spring-ldap.

--- spring-ldap-tiger/src/itest/java/org/springframework/ldap/core/simple/SimpleLdapTemplateITest.java
+++ spring-ldap-tiger/src/itest/java/org/springframework/ldap/core/simple/SimpleLdapTemplateITest.java
@@ -21,7 +21,7 @@ import org.springframework.ldap.AbstractLdapTemplateIntegrationTest;
 import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.DirContextOperations;
 
-public abstract class SimpleLdapTemplateITest extends AbstractLdapTemplateIntegrationTest {
+public class SimpleLdapTemplateITest extends AbstractLdapTemplateIntegrationTest {
 
 	private SimpleLdapTemplate ldapTemplate;
 
