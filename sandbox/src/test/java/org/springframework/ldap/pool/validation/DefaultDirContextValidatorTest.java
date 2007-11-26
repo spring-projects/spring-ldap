12409213785bc14254c902f2bd0a939f31f76aff
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2007-11-26 23:18:10 +0000

    Began restructuring the pooling code.

--- sandbox/src/test/java/org/springframework/ldap/pool/validation/DefaultDirContextValidatorTest.java
+++ sandbox/src/test/java/org/springframework/ldap/pool/validation/DefaultDirContextValidatorTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.ldap.validation;
+package org.springframework.ldap.pool.validation;
 
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
