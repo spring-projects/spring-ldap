2a9958fb4ebf09ad83d8fa9481dc3870983353a2
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2010-11-30 06:57:41 +0000

    Changed to new property skipTests.

--- with-dependencies.sh
+++ with-dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export MAVEN_OPTS=-Xmx512m
-mvn -Dmaven.test.skip=true -P release clean javadoc:javadoc docbkx:generate-html docbkx:generate-pdf install
+mvn -DskipTests -P release clean javadoc:javadoc docbkx:generate-html docbkx:generate-pdf install
