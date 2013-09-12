96429d00c6ef75536b3893bbc9786281d2c644d6
Mattias Hellborg Arthursson <mattias@261consulting.com>
2013-09-12 08:07:15 +0200

    LDAP-262: Moved the stuff from the samples-utils project to plain-sample.

--- samples/samples-utils/src/main/java/org/springframework/ldap/samples/utils/LdapTreeVisitor.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.ldap.samples.utils;
-
-import org.springframework.ldap.core.DirContextOperations;
-
-public interface LdapTreeVisitor {
-
-	public void visit(DirContextOperations node, int currentDepth);
-}
