d84d64f968114d7ddd6bf62c187a13ff23e7531e
Josh Cummings <josh.cummings@gmail.com>
2022-01-13 10:39:40 -0700

    Add asciidoctor task
    Issue gh-613

--- buildSrc/src/main/groovy/io/spring/gradle/convention/DocsPlugin.groovy
+++ buildSrc/src/main/groovy/io/spring/gradle/convention/DocsPlugin.groovy
@@ -1,8 +1,10 @@
 package io.spring.gradle.convention
 
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.plugins.PluginManager
 import org.gradle.api.tasks.bundling.Zip
@@ -17,8 +19,22 @@ public class DocsPlugin implements Plugin<Project> {
 
 		PluginManager pluginManager = project.getPluginManager();
 		pluginManager.apply(BasePlugin);
+		pluginManager.apply("org.asciidoctor.jvm.convert");
 		pluginManager.apply(DeployDocsPlugin);
 		pluginManager.apply(JavadocApiPlugin);
+
+		project.getGradle().afterProject(new Action<Project>() {
+			@Override
+			public void execute(Project p) {
+				RepositoryHandler repositories = p.getRepositories();
+				if (repositories.isEmpty()) {
+					repositories.mavenCentral();
+					repositories.maven(repo -> {
+						repo.setUrl(URI.create("https://repo.spring.io/release"));
+					});
+				}
+			}
+		});
 
 		Task docsZip = project.tasks.create('docsZip', Zip) {
 			dependsOn 'api'
@@ -27,7 +43,10 @@ public class DocsPlugin implements Plugin<Project> {
 			archiveClassifier = 'docs'
 			description = "Builds -${archiveClassifier.get()} archive containing all " +
 				"Docs for deployment at docs.spring.io"
-
+			from(project.tasks.asciidoctor.outputs) {
+				into 'reference'
+				include '**'
+			}
 			from(project.tasks.api.outputs) {
 				into 'api'
 			}
