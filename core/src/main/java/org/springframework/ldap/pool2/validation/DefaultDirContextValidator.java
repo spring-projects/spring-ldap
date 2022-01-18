75fa912f0aabb58fb920e661f87e0007be99f04b
Josh Cummings <josh.cummings@gmail.com>
2022-01-18 13:43:56 -0700

    Polish
    Issue gh-489

--- core/src/main/java/org/springframework/ldap/pool2/validation/DefaultDirContextValidator.java
+++ core/src/main/java/org/springframework/ldap/pool2/validation/DefaultDirContextValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2013 the original author or authors.
+ * Copyright 2005-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -185,7 +185,7 @@ public class DefaultDirContextValidator implements DirContextValidator {
             if (searchResults != null) {
                 try {
                     searchResults.close();
-                } catch (NamingException namingException) {
+                } catch (NamingException ignored) {
                 }
             }
         }
