7dd18fdadd74fb83522a6c85a27f9446d45b82bd
Josh Cummings <josh.cummings@gmail.com>
2023-01-19 12:00:21 -0700

    Remove Unneeded Attribute
    Closes gh-722

--- samples/odm/src/main/java/org/springframework/ldap/samples/plain/domain/Person.java
+++ samples/odm/src/main/java/org/springframework/ldap/samples/plain/domain/Person.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2013 the original author or authors.
+ * Copyright 2005-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,6 @@ public class Person {
 	@Attribute(name = "sn")
 	private String lastName;
 
-	@Attribute(name = "description")
 	private String description;
 
 	@Transient
