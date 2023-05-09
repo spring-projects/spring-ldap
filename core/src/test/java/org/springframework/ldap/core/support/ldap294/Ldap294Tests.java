9418667878d7f08444aee02f89735fa43e4f7128
Josh Cummings <josh.cummings@gmail.com>
2023-05-09 14:06:17 -0600

    Update Copyright for Core Tests
    Issue gh-747

--- core/src/test/java/org/springframework/ldap/core/support/ldap294/Ldap294Tests.java
+++ core/src/test/java/org/springframework/ldap/core/support/ldap294/Ldap294Tests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.ldap.core.support.ldap294;
 
 import java.util.Hashtable;
--- core/src/test/java/org/springframework/ldap/pool2/MutableDelegatingLdapContextTests.java
+++ core/src/test/java/org/springframework/ldap/pool2/MutableDelegatingLdapContextTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.ldap.pool2;
 
 import org.junit.Test;
