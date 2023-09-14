a31ec0660a3aeb915c9ddc99c948309c817ee854
Josh Cummings <josh.cummings@gmail.com>
2023-09-14 09:21:09 -0600

    Update Release Script to Use Cowsay
    Because it's delightful

--- scripts/release/wait-for-done.sh
+++ scripts/release/wait-for-done.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+type cowsay >/dev/null 2>&1 || cowsay() { :; }
+
 VERSION=$1
-until http -h --check-status --ignore-stdin https://repo1.maven.org/maven2/org/springframework/ldap/spring-ldap-core/$VERSION/; do sleep 10; clear; done; spd-say "It is now uploaded"
+until http -h --check-status --ignore-stdin https://repo1.maven.org/maven2/org/springframework/ldap/spring-ldap-core/$VERSION/; do sleep 10; clear; done; spd-say "It is now uploaded" && cowsay "It is now uploaded"
